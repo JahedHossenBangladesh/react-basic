e7b7b8c103df766551fcba04c42951d63c2246e1
JahedHossenBangladesh <jahedhossen4358.com>
2020-12-12 00:39:10 +0600

    this is about 3hours and 17 min

--- README.md
+++ README.md
@@ -100,3 +100,121 @@ const Book = (props) => {
   );
 };
 ```
+
+### the summary of the code
+
+```javascript
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+
+// function Greeting(){
+//    return (<div>
+//      <Person/>
+//      <Message/>
+//      <h4>This is Jahed and this is my component</h4>
+//      </div>);
+// }
+// const Greeting =() =>{
+//   return React.createElement('div',{},React.createElement( 'h1',{},"hello world"));
+// }
+// const Person = () => <h1>this is John</h1>
+// const Message = () =>{
+//   return <p>This is the message</p>
+
+// }
+const firstBook = {
+  author: "by ফরিদ আহমেদ",
+  title: "স্মার্ট ইংলিশ স্মার্ট ওয়ে টু লার্ন ইংলিশ (পেপারব্যাক)",
+  img:
+    "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/46475e27e_173438.jpg",
+};
+
+const secondBook = {
+  author: "Hearts Bangladesh",
+  title: "Hearts General Two Date DIARY - 2021 (Green, Light Green Color)",
+  img:
+    "https://s3-ap-southeast-1.amazonaws.com/rokomari110/ProductNew20190903/260X372/ea13dfd44_205179.jpg",
+};
+function BookList() {
+  return (
+    <section className="bookList">
+      <Book
+        job="developer"
+        img={firstBook.img}
+        author={firstBook.author}
+        title={firstBook.title}
+      >
+        <p>This is in The children</p>{" "}
+      </Book>
+      <Book
+        img={secondBook.img}
+        author={secondBook.author}
+        title={secondBook.title}
+      />
+      <Book make="make by rokomari" price={22} />
+    </section>
+  );
+}
+
+const Book = ({ img, title, author, children }) => {
+  // const {img,title,author} = props;
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1> {title}</h1>
+      <h4 style={{ color: "red", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+        {" "}
+        {author}{" "}
+      </h4>
+      {children}
+      {/* <p>{props.job}</p> */}
+    </article>
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
+```
+
+### Props
+
+```javascript
+function BookList() {
+  return (
+    <section className="bookList">
+      {book.map((books) => {
+        return <Book key={books.id} {...books} />;
+      })}
+    </section>
+  );
+}
+
+const Book = ({ img, title, author }) => {
+  // const {author,title,img} = props;
+};
+```
+
+```javascript
+const eventHadler = (e) =>{
+  console.log(e)
+  console.log(e.target)
+  alert("Hellow wordl")
+}
+const complexHandler = (author,title,img) =>{
+  console.log(author,title,img)
+}
+
+const Book = (props) =>{
+  const {author,title,img} = props;
+
+  return <article className="book" onMouseOver={() =>{
+    console.log(title)
+  }}>
+    <img src={img} alt=""/>
+    <h1 onClick={() => console.log(title)}> {title}</h1>
+    <h4 style={{color:'red' ,fontSize:'0.75rem',marginTop:'0.25rem'}}> {author} </h4>
+   <button type='button' onClick={eventHadler}>reference example</button>
+   <button type='button' onClick={() => complexHandler(author,title,img)}>complex example</button>
+
+ </article>
+```
--- src/index.js
+++ src/index.js
@@ -3,50 +3,62 @@ import ReactDom from 'react-dom'
 import './index.css'
 
 
-
-// function Greeting(){
-//    return (<div>
-//      <Person/>
-//      <Message/>
-//      <h4>This is Jahed and this is my component</h4> 
-//      </div>);
-// }
-// const Greeting =() =>{
-//   return React.createElement('div',{},React.createElement( 'h1',{},"hello world"));
-// }
-// const Person = () => <h1>this is John</h1>
-// const Message = () =>{
-//   return <p>This is the message</p>
-  
-// }
-const firstBook={ author :"by ফরিদ আহমেদ",
- title :'স্মার্ট ইংলিশ স্মার্ট ওয়ে টু লার্ন ইংলিশ (পেপারব্যাক)',
- img :"https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/46475e27e_173438.jpg"
+const  book=[{ 
+  id:1,
+  author :"by David Perkins (Author)",
+ title :'Making Learning Whole: How Seven Principles of Teaching Can Transform Education 1st Edition',
+ img :"https://images-na.ssl-images-amazon.com/images/I/51jpBO3NIAL._AC_SX184_.jpg"
+},
+{ 
+  id:2,
+author:' by Marijn Haverbeke ',
+  title:'Eloquent JavaScript, 3rd Edition: A Modern Introduction to Programming Paperback – Illustrated, December 4, 2018',
+  img:"https://images-na.ssl-images-amazon.com/images/I/51MSWCvCmcL._AC_SX184_.jpg"
+},
+{ 
+  id:3,
+author:'by David Flanagan ',
+  title:'JavaScript: The Definitive Guide: Master the Worlds Most-Used Programming Language 7th Edition',
+  img:'https://images-na.ssl-images-amazon.com/images/I/91hUer84PpL._AC_UL320_SR244,320_.jpg'
+}
+]
+// const name =["jahed","sujon","jon"]
+// const newName = name.map((name) =>{
+//   return <h1>{name}</h1>
+// })
+const eventHadler = (e) =>{
+  console.log(e)
+  console.log(e.target)
+  alert("Hellow wordl")
+}
+const complexHandler = (author,title,img) =>{
+  console.log(author,title,img)
 }
 
-const secondBook={ 
-author:'Hearts Bangladesh',
-  title:'Hearts General Two Date DIARY - 2021 (Green, Light Green Color)',
-  img:'https://s3-ap-southeast-1.amazonaws.com/rokomari110/ProductNew20190903/260X372/ea13dfd44_205179.jpg'
-}
 function BookList(){
   return <section className="bookList">  
-    <Book job='developer' img={firstBook.img} author={firstBook.author} title ={firstBook.title}>
-      <p>This is in The children</p> </Book>
-      <Book img={secondBook.img} author={secondBook.author} title ={secondBook.title}/>
-        <Book make='make by rokomari' price={22}/>
+
+  {
+    book.map((books) =>{
+    
+      return <Book key={books.id} {...books}/>
+    })
+  }
   </section>
 }
 
 
-const Book = ({img,title,author,children}) =>{
-  // const {img,title,author} = props;
-  return <article className="book">
+const Book = (props) =>{
+  const {author,title,img} = props;
+  
+  return <article className="book" onMouseOver={() =>{
+    console.log(title)
+  }}>
     <img src={img} alt=""/>
-    <h1> {title}</h1>
+    <h1 onClick={() => console.log(title)}> {title}</h1>
     <h4 style={{color:'red' ,fontSize:'0.75rem',marginTop:'0.25rem'}}> {author} </h4>
-    {children}
-    {/* <p>{props.job}</p> */}
+   <button type='button' onClick={eventHadler}>reference example</button>
+   <button type='button' onClick={() => complexHandler(author,title,img)}>complex example</button>
   
  </article>
 }
